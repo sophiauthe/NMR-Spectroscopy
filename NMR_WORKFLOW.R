89656a27ba6867d1676971de2325c96e22d7e1ac
Sophia Uthe <sophiauthe@Sophias-MacBook-Pro.local>
2020-10-27 10:09:30 -0400

    update

--- NMR_WORKFLOW.R
+++ NMR_WORKFLOW.R
@@ -3,3 +3,4 @@ data_dir <- system.file("extra", package = "Rnmr1D")
 RAWDIR <- file.path(data_dir, "CD_BBI_16P02")
 CMDFILE <- file.path(data_dir, "NP_macro_cmd.txt")
 SAMPLEFILE <- file.path(data_dir, "Samples.txt")
+change
