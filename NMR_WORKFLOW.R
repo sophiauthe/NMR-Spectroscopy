110b13262c98a2486e21456582d15466a195c87c
Sophia Uthe <sophiauthe@Sophias-MacBook-Pro.local>
2020-10-27 10:04:32 -0400

    created NMR workflow

---
+++ NMR_WORKFLOW.R
@@ -0,0 +1,5 @@
+library(Rnmr1D)
+data_dir <- system.file("extra", package = "Rnmr1D")
+RAWDIR <- file.path(data_dir, "CD_BBI_16P02")
+CMDFILE <- file.path(data_dir, "NP_macro_cmd.txt")
+SAMPLEFILE <- file.path(data_dir, "Samples.txt")
