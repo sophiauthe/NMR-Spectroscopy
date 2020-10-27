6b38c382118d0abdcb3291aaa9a6832a11155b13
Sophia Uthe <sophiauthe@Sophias-MacBook-Pro.local>
2020-10-27 10:35:23 -0400

    update

--- NMR_WORKFLOW.R
+++ NMR_WORKFLOW.R
@@ -1,6 +1,11 @@
 library(Rnmr1D)
-data_dir <- system.file("extra", package = "Rnmr1D")
-RAWDIR <- file.path(data_dir, "CD_BBI_16P02")
-CMDFILE <- file.path(data_dir, "NP_macro_cmd.txt")
-SAMPLEFILE <- file.path(data_dir, "Samples.txt")
-change
+
+RAWDIR <- file.path("SampleData", "SampleData")
+CMDFILE <- file.path("CMDFile", "NP_macro_cmd_NMR_DATA.txt")
+SAMPLEFILE <- file.path("Samples", "NMR_DATA_Samples.txt")
+
+samples <- read.table(SAMPLEFILE, sep="\t", header=T,stringsAsFactors=FALSE)
+samples
+
+CMDTEXT <- readLines(CMDFILE)
+CMDTEXT[grep("^#$", CMDTEXT, invert=TRUE)]
